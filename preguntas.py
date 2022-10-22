273bfa3a8353df42188aae1a1dce108c67a69a78
NataliaPalacio <npalacioq@unal.edu.co>
2022-10-22 15:59:04 -0500

    2
    2

--- preguntas.py
+++ preguntas.py
@@ -43,7 +43,7 @@ def pregunta_02():
     n_iterations = 1000
 
     # Defina el parámetro inicial `params` como un arreglo de tamaño 3 con ceros
-    params = np.zeros(x_poly.shape[1])
+    params = np.zeros(y.shape[1])
     for _ in range(n_iterations):
 
         # Compute el pronóstico con los parámetros actuales
